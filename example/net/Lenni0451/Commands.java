7cd19f9ac6b81c44ca8d0ebf52db2e5df92e9273
Lenni0451 <lenni0451@eclipse>
2019-09-27 22:36:52 +0200

    Improved example

--- example/net/Lenni0451/Commands.java
+++ example/net/Lenni0451/Commands.java
@@ -1,9 +1,13 @@
 package net.Lenni0451;
 
 import net.Lenni0451.commands.Command1;
+import net.Lenni0451.commands.Command2;
+import net.Lenni0451.commands.Command3;
 
 public class Commands {
 	
 	public static Command1 cmd1 = null;
+	public static Command2 cmd2 = null;
+	public static Command3 cmd3 = null;
 	
 }
--- example/net/Lenni0451/Main.java
+++ example/net/Lenni0451/Main.java
@@ -4,16 +4,14 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
 import net.Lenni0451.CommandLib.CommandManager;
-import net.Lenni0451.commands.Command2;
-import net.Lenni0451.commands.Command3;
 
 public class Main {
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, InstantiationException, InvocationTargetException {
 		CommandManager manager = new CommandManager();
 		
 		manager.registerLocalCommands(Commands.class);
-		manager.addCommand(Command2.class, Command3.class);
 		
 		Scanner s = new Scanner(System.in);
 		while(true) {
